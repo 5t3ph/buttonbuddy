dcfdbb08edbb71f3a021d6cf63aaac88f0aa13f6
5t3ph <thinkdobecreate@gmail.com>
2021-01-01 14:06:03 -0600

    Actually fix large text switch

--- src/js/updateRatio.js
+++ src/js/updateRatio.js
@@ -1,7 +1,7 @@
 import updateContrast from "./updateColors";
 
 const ratioSwitch = document.querySelector(".results .switch");
-const ratioColors = document.querySelectorAll("[data-ratio]");
+const ratioColors = document.querySelectorAll('.results strong[id*="color"]');
 
 const updateRatio = (ratio) => {
   ratioColors.forEach((result) => {
